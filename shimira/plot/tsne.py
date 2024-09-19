8112999865b0e1fdf4b2ba8e3d06510e73733c5e
Shoma KANNO <sqrt.trill425@gmail.com>
2024-09-19 21:21:18 +0900

    ラベル付きデータをplotできるように

--- shimira/plot/tsne.py
+++ shimira/plot/tsne.py
@@ -5,11 +5,26 @@ from matplotlib.axes import Axes
 from typing import Optional
 
 
-def tSNE_2Dplot(data: np.ndarray, ax: Optional[Axes] = None, n_jobs: int = 1) -> Axes:
+def tSNE_2Dplot(
+    data: list[np.ndarray], label: list[str], ax: Optional[Axes] = None, n_jobs: int = 1
+) -> Axes:
+
+    assert len(data) == len(label), "入力データ数とラベルの数が合いません"
+
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
 
-    embs = sklearn.manifold.TSNE(n_components=2, n_jobs=n_jobs).fit_transform(data)
+    length_list = [d.shape[0] for d in data]
+    concat_d = np.concatenate(data, axis=0, dtype=np.float32)
 
-    ax.scatter(embs[:, 0], embs[:, 1])
+    embs = sklearn.manifold.TSNE(n_components=2, n_jobs=n_jobs).fit_transform(concat_d)
+
+    idx = 0
+    for i, length in enumerate(length_list):
+        ax.scatter(
+            embs[idx : idx + length, 0], embs[idx : idx + length, 1], label=label[i]
+        )
+        idx += length
+
+    return ax
