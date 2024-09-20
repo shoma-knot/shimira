4373889428ecc34724c5d08316a2c44a9d1db6af
Shoma KANNO <sqrt.trill425@gmail.com>
2024-09-20 10:30:48 +0900

    tSNEを用いた2次元埋め込みを返すだけの関数の実装

--- shimira/plot/tsne.py
+++ shimira/plot/tsne.py
@@ -5,6 +5,10 @@ from matplotlib.axes import Axes
 from typing import Optional
 
 
+def tSNE_2DEmbedding(data: np.ndarray, n_jobs: int = 1) -> np.ndarray:
+    return sklearn.manifold.TSNE(n_components=2, n_jobs=n_jobs).fit_transform(data)
+
+
 def tSNE_2Dplot(
     data: list[np.ndarray], label: list[str], ax: Optional[Axes] = None, n_jobs: int = 1
 ) -> Axes:
@@ -18,7 +22,7 @@ def tSNE_2Dplot(
     length_list = [d.shape[0] for d in data]
     concat_d = np.concatenate(data, axis=0, dtype=np.float32)
 
-    embs = sklearn.manifold.TSNE(n_components=2, n_jobs=n_jobs).fit_transform(concat_d)
+    embs = tSNE_2DEmbedding(data=concat_d, n_jobs=n_jobs)
 
     idx = 0
     for i, length in enumerate(length_list):
